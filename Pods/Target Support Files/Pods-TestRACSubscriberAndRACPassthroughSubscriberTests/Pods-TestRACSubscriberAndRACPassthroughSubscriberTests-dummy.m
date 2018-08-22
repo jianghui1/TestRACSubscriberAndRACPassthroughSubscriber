9895353c17e3335f4e619724e1f716d256c8f44c
jiangzenghui005@163.com <jiangzenghui005@163.com>
2018-08-22 18:57:55 +0800

    测试用例提交。

---
+++ Pods/Target Support Files/Pods-TestRACSubscriberAndRACPassthroughSubscriberTests/Pods-TestRACSubscriberAndRACPassthroughSubscriberTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestRACSubscriberAndRACPassthroughSubscriberTests : NSObject
+@end
+@implementation PodsDummy_Pods_TestRACSubscriberAndRACPassthroughSubscriberTests
+@end
--- TestRACSubscriberAndRACPassthroughSubscriberTests/TestRACSubscriberAndRACPassthroughSubscriberTests.m
+++ TestRACSubscriberAndRACPassthroughSubscriberTests/TestRACSubscriberAndRACPassthroughSubscriberTests.m
@@ -8,32 +8,132 @@
 
 #import <XCTest/XCTest.h>
 
+#import <ReactiveCocoa.h>
+
 @interface TestRACSubscriberAndRACPassthroughSubscriberTests : XCTestCase
 
 @end
 
 @implementation TestRACSubscriberAndRACPassthroughSubscriberTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (RACSignal *)createSignal
+{
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(1)];
+        [subscriber sendCompleted];
+        
+        return nil;
     }];
+}
+
+- (void)testSubscriber
+{
+    [[self createSignal]
+     subscribeNext:^(id x) {
+         NSLog(@"subscriber -- %@", x);
+     }];
+    
+    // 打印日志；
+    /*
+     2018-08-22 17:12:40.849495+0800 TestRACSubscriberAndRACPassthroughSubscriber[41242:3744055] subscriber -- 1
+     */
+}
+
+- (RACSignal *)createSignal1
+{
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(1)];
+        [subscriber sendCompleted];
+        
+        return [RACDisposable disposableWithBlock:^{
+            NSLog(@"woguale");
+        }];
+    }];
+}
+
+- (void)testSubscriber1
+{
+    RACDisposable *disposable = [[self createSignal1]
+                                 subscribeNext:^(id x) {
+                                     NSLog(@"subscriber -- %@", x);
+                                 }];
+    [disposable dispose];
+    // 打印日志；
+    /*
+     2018-08-22 18:05:48.451933+0800 TestRACSubscriberAndRACPassthroughSubscriber[3006:75065] subscriber -- 1
+     2018-08-22 18:05:48.452257+0800 TestRACSubscriberAndRACPassthroughSubscriber[3006:75065] woguale
+     */
+}
+
+- (RACSignal *)createSignal2
+{
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [subscriber sendNext:@(1)];
+        [subscriber sendCompleted];
+        
+        return [RACDisposable disposableWithBlock:^{
+            NSLog(@"woguale");
+        }];
+    }];
+}
+
+- (void)testSubscriber2
+{
+    [[self createSignal2]
+     subscribeNext:^(id x) {
+         NSLog(@"subscriber -- %@", x);
+     }];
+    
+    // 打印日志；
+    /*
+     2018-08-22 18:14:53.746384+0800 TestRACSubscriberAndRACPassthroughSubscriber[3310:100752] subscriber -- 1
+     2018-08-22 18:14:53.746773+0800 TestRACSubscriberAndRACPassthroughSubscriber[3310:100752] woguale
+     */
+}
+
+- (void)testSubscriber3
+{
+    RACSignal *signal = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [[RACScheduler scheduler] afterDelay:0.3 schedule:^{
+            [subscriber sendNext:@(1)];
+            [subscriber sendCompleted];
+            
+        }];
+        
+        return [RACDisposable disposableWithBlock:^{
+            NSLog(@"woguale");
+        }];
+    }];
+    
+    [signal subscribeNext:^(id x) {
+        NSLog(@"signal");
+    }];
+    
+    RACSignal *signal1 = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [[RACScheduler scheduler] afterDelay:0.3 schedule:^{
+            [subscriber sendNext:@(1)];
+            [subscriber sendCompleted];
+            
+        }];
+        
+        return [RACDisposable disposableWithBlock:^{
+            NSLog(@"woguale1");
+        }];
+    }];
+    
+    RACDisposable *disposable = [signal1 subscribeNext:^(id x) {
+        NSLog(@"signal1");
+    }];
+    [disposable dispose];
+    
+    [[RACSignal never] asynchronouslyWaitUntilCompleted:nil];
+    
+    // 打印日志：
+    /*
+     2018-08-22 18:50:47.630042+0800 TestRACSubscriberAndRACPassthroughSubscriber[4529:202051] woguale1
+     2018-08-22 18:50:47.931631+0800 TestRACSubscriberAndRACPassthroughSubscriber[4529:202084] signal
+     2018-08-22 18:50:47.932003+0800 TestRACSubscriberAndRACPassthroughSubscriber[4529:202084] woguale
+     */
 }
 
 @end
